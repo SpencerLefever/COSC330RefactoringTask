688574782ee8010ef61497119ece6bcf04172147
Spencer Lefever <slefever1@gulls.salisbury.edu>
2022-12-11 21:12:47 -0500

    Phase 2 steps 9+10

--- src/main/java/bad/robot/refactoring/chapter1/Movie.java
+++ src/main/java/bad/robot/refactoring/chapter1/Movie.java
@@ -11,19 +11,46 @@ public class Movie {
 
     public Movie(String title, int priceCode) {
         this.title = title;
+        setPriceCode(priceCode);
+    }
+
+    public void setPriceCode(int priceCode) {
         this.priceCode = priceCode;
     }
+
 
     public String getTitle() {
         return title;
     }
 
-    public int getPriceCode() {
-        return priceCode;
+    public double getCharge(int daysRented) {
+        double thisAmount = 0;
+
+        switch (priceCode) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+
+        return thisAmount;
     }
 
-    public void setPriceCode(int priceCode) {
-        this.priceCode = priceCode;
+    public int getFrequentRenterPoints(int daysRented) {
+            // add bonus for a two day new release rental
+            if (priceCode == Movie.NEW_RELEASE && daysRented > 1)
+                return 2;
+
+        return 1;
     }
 
 }
