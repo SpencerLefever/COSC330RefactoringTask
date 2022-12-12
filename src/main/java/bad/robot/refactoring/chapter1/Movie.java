246ea33dab1bf33c21b2f219a1e257aeeaf0b362
Spencer Lefever <slefever1@gulls.salisbury.edu>
2022-12-11 21:24:45 -0500

    Phase 2 up to step 13

--- src/main/java/bad/robot/refactoring/chapter1/Movie.java
+++ src/main/java/bad/robot/refactoring/chapter1/Movie.java
@@ -7,47 +7,45 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String title;
-    private int priceCode;
+    //private int priceCode;
+    private Price price;
 
     public Movie(String title, int priceCode) {
         this.title = title;
         setPriceCode(priceCode);
     }
 
-    public void setPriceCode(int priceCode) {
-        this.priceCode = priceCode;
+    private void setPriceCode(int priceCode) {
+        switch (priceCode) {
+            case CHILDREN:
+                price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                price = new NewReleasePrice();
+                break;
+            case REGULAR:
+                price = new RegularPrice();
+                break;
+            default:
+                throw new IllegalArgumentException("invalid price code");
+        }
     }
 
+    private int getPriceCode() {
+        return price.getPriceCode();
+    }
 
     public String getTitle() {
         return title;
     }
 
     public double getCharge(int daysRented) {
-        double thisAmount = 0;
-
-        switch (priceCode) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case Movie.CHILDREN:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
-                break;
-        }
-
-        return thisAmount;
+        return price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
             // add bonus for a two day new release rental
-            if (priceCode == Movie.NEW_RELEASE && daysRented > 1)
+            if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
                 return 2;
 
         return 1;
--- src/main/java/bad/robot/refactoring/chapter1/Price.java
+++ src/main/java/bad/robot/refactoring/chapter1/Price.java
@@ -1,4 +1,26 @@
 package bad.robot.refactoring.chapter1;
 public abstract class Price {
     abstract int getPriceCode();
+
+    public double getCharge(int daysRented) {
+        double thisAmount = 0;
+
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+
+        return thisAmount;
+    }
 }
